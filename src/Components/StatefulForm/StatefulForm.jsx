c95b16bf33326663c8bb5159429113a2c7d342c8
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-06-03 22:02:07 +0600

    password validation and error handleing

--- src/Components/StatefulForm/StatefulForm.jsx
+++ src/Components/StatefulForm/StatefulForm.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 
 
 const StatefulForm = () => {
-    const [name, setName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [ password,setPassword] = useState(null)
+    const [name, setName] = useState('Enter you name');
+    const [email, setEmail] = useState('Enter Your Email');
+    const [ password,setPassword] = useState(null);
+    const [error, setError] =useState('');
+
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(name, email,password)
+        
+        if(password.length < 6){
+            setError('password must be 6 characters')
+        }
+        else{
+            setError('')
+            console.log(name, email,password);
+
+        }
     }
     const handleNameChange = e => {
         setName(e.target.value);
@@ -18,19 +28,23 @@ const StatefulForm = () => {
     }
     const handlePasswordChange =e =>{
         setPassword(e.target.value);
+      
         
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <p>name <input onChange={handleNameChange} type="text" name="name" /></p>
+                <p>name <input onChange={handleNameChange} type="text" name="name" value={name} /></p>
                 <p>email <input
-                    onChange={handleEmailChange} type="email" name="email" /></p>
+                    onChange={handleEmailChange} type="email" name="email" value={email}/></p>
                 <p>password <input 
-                onChange={handlePasswordChange} type="password" name="password" /></p>
+                onChange={handlePasswordChange} type="password" name="password" required/></p>
                 <input type="submit" value="submit" />
 
             </form>
+            {
+                error && <p> {error}</p>
+            }
         </div>
     );
 };
